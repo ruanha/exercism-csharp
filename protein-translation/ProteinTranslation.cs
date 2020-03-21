34bfcb380d65f0489eff231c2b9efcf3292f2a39
ruanha <rune_hartvig@hotmail.com>
2020-03-21 15:25:59 +0100

    first iteration

--- protein-translation/ProteinTranslation.cs
+++ protein-translation/ProteinTranslation.cs
@@ -1,9 +1,25 @@
 ﻿using System;
-
+using System.Collections.Generic;
+/*
+Codon 	Protein
+AUG 	Methionine
+UUU, UUC 	Phenylalanine
+UUA, UUG 	Leucine
+UCU, UCC, UCA, UCG 	Serine
+UAU, UAC 	Tyrosine
+UGU, UGC 	Cysteine
+UGG 	Tryptophan
+UAA, UAG, UGA 	STOP
+*/
 public static class ProteinTranslation
 {
+    enum AminoAcid {
+        Methionine, Phenylalanine, Leucine, Serine,Tyrosine, Cysteine, Tryptophan, STOP
+    }
+
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException();
+        var result = new string[1]{"Methionine"};
+        return result;
     }
 }
