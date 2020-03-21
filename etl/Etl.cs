a702ae777adf0e9ea047bc31eb8455f3b45b4ce9
ruanha <rune_hartvig@hotmail.com>
2020-03-21 14:42:27 +0100

    add transform algo

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -5,6 +5,14 @@ public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<int, string[]> entry in old)
+        {
+            foreach (string letter in entry.Value) {
+                result.Add(letter.ToLower(), entry.Key);
+            }
+        }
+        return result;
     }
 }
