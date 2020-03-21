54da418c3cb76cf3e91ae3b3af74035d9ee14cc9
ruanha <rune_hartvig@hotmail.com>
2020-03-21 17:29:43 +0100

    moves dict and enum to bottom of class

--- protein-translation/ProteinTranslation.cs
+++ protein-translation/ProteinTranslation.cs
@@ -2,35 +2,12 @@
 
 public static class ProteinTranslation
 {
-    enum AminoAcids {
-        Methionine, Phenylalanine, Leucine, Serine, Tyrosine, Cysteine, Tryptophan, STOP
-    }
-    private static Dictionary<string, string> Codon = new Dictionary<string, string>{
-        {"AUG", AminoAcids.Methionine.ToString()},
-        {"UUU", AminoAcids.Phenylalanine.ToString()},
-        {"UUC", AminoAcids.Phenylalanine.ToString()},
-        {"UUA", AminoAcids.Leucine.ToString()},
-        {"UUG", AminoAcids.Leucine.ToString()},
-        {"UCU", AminoAcids.Serine.ToString()},
-        {"UCC", AminoAcids.Serine.ToString()},
-        {"UCA", AminoAcids.Serine.ToString()},
-        {"UCG", AminoAcids.Serine.ToString()},
-        {"UAU", AminoAcids.Tyrosine.ToString()},
-        {"UAC", AminoAcids.Tyrosine.ToString()},
-        {"UGU", AminoAcids.Cysteine.ToString()},
-        {"UGC", AminoAcids.Cysteine.ToString()},
-        {"UGG", AminoAcids.Tryptophan.ToString()},
-        {"UAA", AminoAcids.STOP.ToString()},
-        {"UAG", AminoAcids.STOP.ToString()},
-        {"UGA", AminoAcids.STOP.ToString()},
-    };
-
-    public static string[] Proteins(string strand)
+   public static string[] Proteins(string strand)
     {
-        List<string> chunk = ChunkUp(strand, 3);
+        List<string> codons = ChunkUp(strand, 3);
         List<string> result = new List<string>();
         
-        foreach (string codon in chunk) {
+        foreach (string codon in codons) {
             string AA = GetAminoAcidFrom(codon);
             if (AA != AminoAcids.STOP.ToString()) {
                 result.Add(AA);
@@ -51,4 +28,28 @@ public static class ProteinTranslation
     private static string GetAminoAcidFrom(string codon) {
         return Codon[codon];
     }
+
+    enum AminoAcids {
+        Methionine, Phenylalanine, Leucine, Serine, Tyrosine, Cysteine, Tryptophan, STOP
+    }
+ 
+    private static Dictionary<string, string> Codon = new Dictionary<string, string>{
+        {"AUG", AminoAcids.Methionine.ToString()},
+        {"UUU", AminoAcids.Phenylalanine.ToString()},
+        {"UUC", AminoAcids.Phenylalanine.ToString()},
+        {"UUA", AminoAcids.Leucine.ToString()},
+        {"UUG", AminoAcids.Leucine.ToString()},
+        {"UCU", AminoAcids.Serine.ToString()},
+        {"UCC", AminoAcids.Serine.ToString()},
+        {"UCA", AminoAcids.Serine.ToString()},
+        {"UCG", AminoAcids.Serine.ToString()},
+        {"UAU", AminoAcids.Tyrosine.ToString()},
+        {"UAC", AminoAcids.Tyrosine.ToString()},
+        {"UGU", AminoAcids.Cysteine.ToString()},
+        {"UGC", AminoAcids.Cysteine.ToString()},
+        {"UGG", AminoAcids.Tryptophan.ToString()},
+        {"UAA", AminoAcids.STOP.ToString()},
+        {"UAG", AminoAcids.STOP.ToString()},
+        {"UGA", AminoAcids.STOP.ToString()},
+    };
 }
