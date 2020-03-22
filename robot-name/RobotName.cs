cc2fd7ddf2e16701fad6c1eee9d7bbe4636a6b91
ruanha <rune_hartvig@hotmail.com>
2020-03-22 11:29:00 +0100

    add name field

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -2,11 +2,15 @@ using System;
 
 public class Robot
 {
+    private string _name;
+    public Robot() {
+        _name = "AB123";
+    }
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return _name;
         }
     }
 
