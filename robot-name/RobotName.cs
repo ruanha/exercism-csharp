6d168f07e1f24636139b0e7b309d8f79c1800c53
ruanha <rune_hartvig@hotmail.com>
2020-03-22 12:31:00 +0100

    guarante that name is different after reset

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,45 +1,40 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 public class Robot
 {
-    static HashSet<string> _names = new HashSet<string>();
+    private static HashSet<string> _names = new HashSet<string>();
     private string _name;
-    private string UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public Robot() {
         _name = GenerateName();
+        _names.Add(_name);
     }
-    public string Name
+    public string Name { get => _name; }
+    public void Reset()
     {
-        get
-        {
-            return _name;
-        }
+        _name = GenerateName();
+        _names.Remove(_name);
+        _names.Add(_name);
     }
 
     private string GenerateName() {
         var suggestedName = GenerateRandomLetter() + GenerateRandomLetter() + GenerateRandomNumeric();
         if (!Robot._names.Contains(suggestedName)) {
-            _names.Add(suggestedName);
             return suggestedName;
         }
         return GenerateName();
     }
 
     private string GenerateRandomLetter() {
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";;
         Random random = new Random();
-        var r = random.Next(UpperCaseLetters.Length);
-        return UpperCaseLetters[r].ToString();
+        var r = random.Next(letters.Length);
+        return letters[r].ToString();
     }
 
     private string GenerateRandomNumeric() {
         Random random = new Random();
         return random.Next(100, 1000).ToString();
-    }
-
-    public void Reset()
-    {
-        _name = GenerateName();
     }
 }
