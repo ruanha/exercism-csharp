163846b026ebcff49ca3dfc3b4b9effd62743bb3
ruanha <rune_hartvig@hotmail.com>
2020-03-22 12:47:54 +0100

    make random letter generator return multiple

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -18,17 +18,20 @@ public class Robot
     }
 
     private string GenerateName() {
-        var suggestedName = GenerateRandomLetter() + GenerateRandomLetter() + GenerateRandomNumeric();
+        var suggestedName = GenerateRandomLetters(2) + GenerateRandomNumeric();
         if (!Robot._names.Contains(suggestedName)) {
             return suggestedName;
         }
         return GenerateName();
     }
 
-    private string GenerateRandomLetter() {
-        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";;
-        var r = random.Next(letters.Length);
-        return letters[r].ToString();
+    private string GenerateRandomLetters(int length) {
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string result = "";
+        for (int i = 0; i < length; i++) {
+            result += letters[random.Next(letters.Length)].ToString();
+        }
+        return result;
     }
 
     private string GenerateRandomNumeric() {
