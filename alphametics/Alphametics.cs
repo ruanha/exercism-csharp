e2d385cf55df0c59512204efab7033ac8d0b93f4
ruanha <rune_hartvig@hotmail.com>
2020-03-22 22:01:49 +0100

    add initial parsing of the two sides of the equation

--- alphametics/Alphametics.cs
+++ alphametics/Alphametics.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class Alphametics
 {
+    private static string RightHand;
+    private static string[] LeftHand;
     public static IDictionary<char, int> Solve(string equation)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Parse(equation);
+        return new Dictionary<char, int>
+        {
+            ['I'] = 1,
+            ['B'] = 9,
+            ['L'] = 0
+        };
+    }
+
+    private static void Parse(string equation) {
+        SetRightHand(equation.Split("==")[1]);
+        SetLeftHand(equation.Split("==")[0]);
+    }
+
+    private static void SetRightHand(string untrimmed) {
+        RightHand = untrimmed.Trim();
+    }
+
+    private static void SetLeftHand(string unsplitAndUntrimmed) {
+        string[] untrimmed = unsplitAndUntrimmed.Split("+");
+        LeftHand = untrimmed
+            .Select(x => x.Trim())
+            .ToArray();
     }
 }
