129a7b6f78b7ce29c6079b731801943e9f934a38
ruanha <rune_hartvig@hotmail.com>
2020-03-23 20:54:05 +0100

    add mask check for allergy

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -1,26 +1,40 @@
 using System;
 
+/*
+1  = 0000 0001
+2  = 0000 0010
+4  = 0000 0100
+8  = 0000 1000
+16 = 0001 0000
+32 = 0010 0000
+64 = 0100 0000
+128= 1000 0000
+*/
+
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
+    private int Mask;
     public Allergies(int mask)
     {
+        Mask = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = Mask & (byte)allergen;
+        return result == (byte)allergen;
     }
 
     public Allergen[] List()
