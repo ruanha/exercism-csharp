2809c599d419c3199030a7c91cda4034529cb24f
ruanha <rune_hartvig@hotmail.com>
2020-03-23 21:33:07 +0100

    inline more variables, oneline methods

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -3,30 +3,18 @@ using System.Collections.Generic;
 
 public enum Allergen
 {
-    Eggs          = 1,
-    Peanuts       = 2,
-    Shellfish     = 4,
-    Strawberries  = 8,
-    Tomatoes      = 16,
-    Chocolate     = 32,
-    Pollen        = 64,
-    Cats          = 128
+    Eggs = 1, Peanuts = 2, Shellfish = 4, Strawberries = 8, 
+    Tomatoes = 16, Chocolate = 32, Pollen = 64, Cats = 128,
 }
 
 public class Allergies
 {
     private readonly int Mask;
 
-    public Allergies(int mask)
-    {
-        Mask = mask;
-    }
+    public Allergies(int mask) => Mask = mask;
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        var result = Mask & (byte)allergen;
-        return result == (byte)allergen;
-    }
+    public bool IsAllergicTo(Allergen allergen) =>
+        (Mask & (byte)allergen) == (byte)allergen;
 
     public Allergen[] List()
     {
