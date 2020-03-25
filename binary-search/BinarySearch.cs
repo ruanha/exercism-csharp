b0a2776d86f4a7b68f0830ba18ba9aa28eac04cf
ruanha <rune_hartvig@hotmail.com>
2020-03-25 14:57:27 +0100

    find value in middle of array

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (input.Length == 0) {
+            return -1;
+        }
+
+        int index = input.Length / 2;
+
+        while (input[index] != value) {
+            
+        }
+
+        return index;
     }
 }
