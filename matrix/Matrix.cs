b3985818807cbfc1872c2cb7563ddd4b4c8f8720
ruanha <rune_hartvig@hotmail.com>
2020-03-26 16:07:14 +0100

    initial Matrix

---
+++ matrix/Matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Matrix
+{
+    public Matrix(string input)
+    {
+
+    }
+
+    public int Rows
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public int Cols
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public int[] Row(int row)
+    {
+        return new int[] {1};
+    }
+
+    public int[] Column(int col)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
