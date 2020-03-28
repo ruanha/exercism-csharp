8a8bcb0534bef74cfd4221d8a90963897cef6e92
ruanha <rune_hartvig@hotmail.com>
2020-03-28 20:25:05 +0100

    prints results row

--- alphametics/Alphametics.cs
+++ alphametics/Alphametics.cs
@@ -4,6 +4,38 @@ using System.Collections.Generic;
 
 public static class Alphametics
 {
+    public static IDictionary<char, int> Solve(string equation)
+    {
+        Matrix matrix = new Matrix(equation.Split("==")[0]);
+        Matrix resultRow = new Matrix(equation.Split("==")[1]);
+        Print(matrix, resultRow);
+        //return Search(matrix, resultRow;
+        return new Dictionary<char, int>();
+    }
+
+    public static void Print(Matrix matrix, Matrix resultRow) {
+        Console.WriteLine("Print the matrix and result row");
+        var numberOfRows = matrix.Column(0).Count();
+        var numberOfColumns = matrix.Row(0).Count();
+        var numberOfColumnsInResult = resultRow.Row(0).Count();
+
+        for (int i = 0; i < numberOfRows; i++ ) {
+            for (int k = 0; k < numberOfColumnsInResult - numberOfColumns; k++) {
+                Console.Write("| ");
+            }
+            for (int j = 0; j < numberOfColumns; j++) {
+                var l = matrix.Cell(i,j).Letter != null ? matrix.Cell(i,j).Letter.ToString() : " ";
+                Console.Write("|" + l);
+            }
+            Console.WriteLine("|");
+        }
+        Console.WriteLine("-------");
+        foreach (Unit u in resultRow.Row(0)) {
+            Console.Write("|" + u.Letter);
+        }
+        Console.WriteLine("|");
+    }
+
     public class Unit {
         public char? Letter;
         public List<int> Values;
@@ -14,33 +46,11 @@ public static class Alphametics
         }
     }
 
-
-    public static IDictionary<char, int> Solve(string equation)
-    {
-        Matrix matrix = new Matrix(equation.Split("==")[0]);
-        Print(matrix);
-        return new Dictionary<char, int>();
-    }
-
-    public static void Print(Matrix matrix) {
-        Console.WriteLine("Print the matrix and result row");
-        var numberOfRows = matrix.Column(0).Count();
-        var numberOfColumns = matrix.Row(0).Count();
-        for (int i = 0; i < numberOfRows; i++ ) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                var l = matrix.Cell(i,j).Letter != null ? matrix.Cell(i,j).Letter.ToString() : " ";
-                Console.Write("|" + l);
-            }
-            Console.WriteLine("|");
-        }
-    }
-
     public class Matrix
     {
         private readonly List<Unit[]> _rows = new List<Unit[]>();
 
         public Matrix(string input)
-        // "I + BB"
         {
             var temp = input
                 .Split("+")
