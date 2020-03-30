72855b66b1433a751b3a189555d7e2a24b198238
ruanha <rune_hartvig@hotmail.com>
2020-03-30 21:54:36 +0200

    move to property and lambda main function

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,25 +1,17 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class RotationalCipher
 {
-    public static string Rotate(string text, int shiftKey)
-    {
-        var result = "";
-        foreach (char c in text) {
-            result += Rotate(c, shiftKey);
-        }
-        return result;
-    }
+    static LinkedList<char> Alphabet = new LinkedList<char>("abcdefghijklmnopqrstuvwxyz".ToCharArray());
+    public static string Rotate(string text, int shiftKey) =>
+        text.Aggregate("", (current, next) => current + Rotate(next, shiftKey));
 
     private static char Rotate(char c, int shiftKey) {
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-        LinkedList<char> linkedAlphabet = new LinkedList<char>(alphabet);
-
-        int i;
-        var node = linkedAlphabet.Find(c);
-        for (i = 0; i < shiftKey; i++) {
-            node = node.Next ?? linkedAlphabet.First;
+        var node = Alphabet.Find(c);
+        for (int i = 0; i < shiftKey; i++) {
+            node = node.Next ?? Alphabet.First;
         }
         return (char)(node.Value);
     }
