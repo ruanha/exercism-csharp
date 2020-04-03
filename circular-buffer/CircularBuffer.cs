986afde3234ac9c8bde63e8d709b3a642ecd538a
ruanha <rune_hartvig@hotmail.com>
2020-04-03 19:06:47 +0200

    initial commit

---
+++ circular-buffer/CircularBuffer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class CircularBuffer<T>
+{
+    public CircularBuffer(int capacity)
+    {
+        
+    }
+
+    public T Read()
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void Write(T value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public void Overwrite(T value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
